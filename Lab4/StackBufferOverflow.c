417879b0d427f1a7c35a76ecd99f6ab8577c74cc
sp1end0r <ssinus.zk@gmail.com>
2021-12-01 16:21:17 +0900

    scanf -> read

--- Lab4/StackBufferOverflow.c
+++ Lab4/StackBufferOverflow.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(){
 	char input[32];
 	char scret[32] = "This is key strings";
 
 	printf("input your string: ");
-	scanf("%s", input);
+	read(0, input, 100);
 	printf("input data = %s\n", input);
 	printf("scret data = %s\n", scret);
 
